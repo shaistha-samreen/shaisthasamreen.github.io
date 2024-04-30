83c4f9e0840665aa6357f0e311e4f5c37cd2dbe6
shaistha-samreen <shaisthasamreen786@gmail.com>
2024-05-01 02:23:03 +0530

    updated ui

--- app/component/sider.jsx
+++ app/component/sider.jsx
@@ -23,7 +23,7 @@ const Sider = () => {
         <div className="flex flex-col h-full justify-center">
           <div className='flex flex-col space-y-4 text-left lg:text-left sm:text-left'>
             {/* Icon and Name/Button */}
-            <div className='flex flex-row lg:flex-row lg:justify-end items-center lg:gap-6 gap-8 sm:gap-8 sm:justify-center justify-center'>
+            <div className='flex flex-row lg:flex-row lg:justify-end items-center lg:gap-6 gap-6 sm:gap-6 sm:justify-center justify-center'>
               {/* Icons */}
               <div className='lg:flex lg:flex-col lg:gap-4 sm:gap-3 gap-3 justify-center sm:justify-center '>
                 {/* Github Icon */}
