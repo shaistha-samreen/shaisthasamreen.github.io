880a166cc740b6d20d26722140f4c4e8ee01924a
shaistha-samreen <shaisthasamreen786@gmail.com>
2024-05-01 02:14:29 +0530

    modified the ui

--- app/component/sider.jsx
+++ app/component/sider.jsx
@@ -47,9 +47,9 @@ const Sider = () => {
               {/* Name and Button */}
               <div className='flex flex-col lg:gap-4 sm:gap-2 gap-2  justify-center items-center'>
                 <div className='flex flex-col lg:gap-4 sm:gap-2 gap-2'>
-                  <h1 className='text-white  sm:text-3xl lg:text-4xl text-3xl font-extrabold'>Hi, Myself <br />Shaistha Samreen</h1>
-                  <p className='text-purple-500 sm:text-base lg:text-base text-base font-semibold'>And I'm a Frontend Developer</p>
-                  <button className="bg-gradient-to-br from-cyan-400 via-purple-600 to-pink-400 rounded-full border-white text-black border font-bold text-sm button h-8 lg:w-24 sm:w-64 w-64" >Access CV</button>
+                  <h1 className='text-white  sm:text-2xl lg:text-4xl text-2xl font-extrabold'>Hi, Myself <br />Shaistha Samreen</h1>
+                  <p className='text-purple-500 sm:text-sm lg:text-base text-sm font-semibold'>And I'm a Frontend Developer</p>
+                  <button className="bg-gradient-to-br from-cyan-400 via-purple-600 to-pink-400 rounded-full border-white text-black border font-bold lg:text-sm sm:text-xs text-xs button h-8 lg:w-24 sm:w-64 w-64" >Access CV</button>
 
                 </div>
               </div>
