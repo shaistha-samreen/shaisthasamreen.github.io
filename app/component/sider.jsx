523195a6999e0675b939a1fdfd769a6fe6774b5b
shaistha-samreen <shaisthasamreen786@gmail.com>
2024-05-01 02:08:47 +0530

    modified the ui

--- app/component/sider.jsx
+++ app/component/sider.jsx
@@ -18,10 +18,10 @@ const Sider = () => {
   return (
     <div className="flex flex-col lg:flex-row h-screen justify-center items-center" onWheel={handleScroll} style={{ overflow: 'hidden' }}>
       {/* Sidebar and Content Container */}
-      <div className="lg:w-2/6 sm:w-full w-full overflow-y-auto" ref={containerRef} style={{ overflowY: 'auto', scrollbarWidth: 'none', height: '40vh' }}>
+      <div className="lg:w-2/6 sm:w-full w-full overflow-y-auto" ref={containerRef} style={{ overflowY: 'auto', scrollbarWidth: 'none', height: '38vh' }}>
         {/* Sidebar */}
         <div className="flex flex-col h-full justify-center">
-          <div className='flex flex-col space-y-4 text-center lg:text-left sm:text-center'>
+          <div className='flex flex-col space-y-4 text-left lg:text-left sm:text-left'>
             {/* Icon and Name/Button */}
             <div className='flex flex-row lg:flex-row lg:justify-end items-center lg:gap-6 gap-4 sm:gap-4 sm:justify-center justify-center'>
               {/* Icons */}
@@ -49,7 +49,7 @@ const Sider = () => {
                 <div className='flex flex-col lg:gap-4 sm:gap-2 gap-2'>
                   <h1 className='text-white  sm:text-3xl lg:text-4xl text-3xl font-extrabold'>Hi, Myself <br />Shaistha Samreen</h1>
                   <p className='text-purple-500 sm:text-base lg:text-base text-base font-semibold'>And I'm a Frontend Developer</p>
-                  <button className="bg-gradient-to-br from-cyan-400 via-purple-600 to-pink-400 rounded-full border-white text-black border font-bold text-sm button h-8 lg:w-24 sm:w-72 w-72" >Access CV</button>
+                  <button className="bg-gradient-to-br from-cyan-400 via-purple-600 to-pink-400 rounded-full border-white text-black border font-bold text-sm button h-8 lg:w-24 sm:w-64 w-64" >Access CV</button>
 
                 </div>
               </div>
